b96b5c5de334ad09bd97b903a6ec79637deb65dc
SharmaGSaroj <91160959+SharmaGSaroj@users.noreply.github.com>
2022-11-04 18:40:15 -0400

    First Commit

---
+++ js/components/TheNavigation.js
@@ -0,0 +1,34 @@
+export default {
+    name: "TheNavigation",
+
+    props: ['item'],
+
+    template: `
+    <header class="header">
+        <nav class="navbar">
+            <a href="#" class="nav-logo"><img src="images/logo-black.svg" alt="" srcset="" /></a>
+            <ul class="nav-menu">
+                <li class="nav-item">
+                    <a href="#" class="nav-link">Home</a>
+                </li>
+                <li class="nav-item">
+                    <a href="#" class="nav-link">Portfolio</a>
+                </li>
+                <li class="nav-item">
+                    <a href="#" class="nav-link">About</a>
+                </li>
+                <li class="nav-item">
+                    <a href="#" class="nav-link">Contact</a>
+                </li>
+            </ul>
+            <div class="hamburger">
+                <span class="bar"></span>
+                <span class="bar"></span>
+                <span class="bar"></span>
+            </div>
+        </nav>
+    </header>
+    
+    
+    `,
+}
